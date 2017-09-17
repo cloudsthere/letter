c63dd77a4f69ec9a81e1dcd3823762c983985754
liang <cloudsthere@163.com>
2017-09-17 05:13:08 +0000

    init

---
+++ letter.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+liang = Programmer()
+xian = PrettyGirl()
+
+time = Zazhiyun.begin('创新谷').to('M+创工场').together()
+
+while time:
+    if xiang.happy() is True :
+        liang.son.shining()
+
+
+if __name__ == '__main__':
+    liang.liao('约?');
+
